32c3600b795476337f9bd3f6ba0c784eef14a655
giavanxilin123 <1614058@hcmut.edu.vn>
2022-03-14 15:41:01 +0700

    add file exchange

--- ecommerce-admin/src/redux/actions/exchange.js
+++ ecommerce-admin/src/redux/actions/exchange.js
@@ -4,18 +4,18 @@ import { toast } from 'react-toastify';
 import { actShowLoading, actHiddenLoading } from './loading'
 import 'react-toastify/dist/ReactToastify.css';
 
-export const actFetchExchangeRequest = (id) => {
+export const actFetchExchangeRequest = (id, token) => {
 //   const newOffset = offset === null || offset === undefined ? 0 : offset;
 //   const limit = 10;
   let payload = {"id" : id, "type": "reqUserName"}
   return dispatch => {
     dispatch(actShowLoading());
     return new Promise((resolve, reject) => {
-      callApi(`exchange`, 'POST', payload)
+      callApi(`exchange`, 'POST', payload, token)
         .then(res => {
           if (res && res.status === 200) { 
-              console.log('req', res.data)
-            // dispatch(actFetchExchange(res.data));
+            //   console.log('req', res.data)
+            dispatch(actFetchExchange(res.data));
             resolve(res.data);
             setTimeout(function(){ dispatch(actHiddenLoading()) }, 200);
           }
@@ -28,13 +28,6 @@ export const actFetchExchangeRequest = (id) => {
     });
   };
 };
-
-// export const actFetchProducts = (products) => {
-//   return {
-//     type: Types.FETCH_PRODUCTS,
-//     products
-//   }
-// }
 
 
 
