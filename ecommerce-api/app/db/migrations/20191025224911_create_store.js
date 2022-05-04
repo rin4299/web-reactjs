f10be8a66156a8cc610b9b93fa436b747dc3201d
minh26799 <minhnguyen1752348@gmail.com>
2022-05-04 06:50:47 -0700

    change type of lat and lng of Store information

--- ecommerce-api/app/db/migrations/20191025224911_create_store.js
+++ ecommerce-api/app/db/migrations/20191025224911_create_store.js
@@ -4,8 +4,8 @@ exports.up = function(knex) {
   return knex.schema.createTable('store', (table) => {
     table.increments('id').primary();
     table.string('storeName');
-    table.float('lat');
-    table.float('lng');
+    table.double('lat');
+    table.double('lng');
     table.string('address');
     table.string('phone');
     table.timestamp('createdAt');
